2325563d94f5fdaf1bef7b51c5847e0ce19eb748
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-18 14:51:14 +0700

    Complete People Skill

--- migrations/20170215053631-create-users-skill.js
+++ migrations/20170215053631-create-users-skill.js
@@ -8,10 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userID: {
+      UserId: {
         type: Sequelize.INTEGER
       },
-      skillID: {
+      SkillId: {
         type: Sequelize.INTEGER
       },
       nilai: {
--- models/users_skill.js
+++ models/users_skill.js
@@ -1,6 +1,13 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Users_skill = sequelize.define('Users_skill', {
+    //id dibawah ini adalah tambahan jika saat menampilkan data Users_skill idnya tidak muncul
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     UserId: DataTypes.INTEGER,
     SkillId: DataTypes.INTEGER,
     nilai: DataTypes.INTEGER
